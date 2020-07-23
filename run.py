ddac90cfa20c5b7f03d4b0f8d1c41d354d5782df
Canberk Hallik <canberkh@gmail.com>
2020-07-23 15:14:11 +0200

    add header, response_model, and status_code to endpoints

--- run.py
+++ run.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Header
 from models.user import User
 from models.book import Book
 from models.author import Author
+from starlette.status import HTTP_201_CREATED
 
 app = FastAPI()
 
 
-@app.post("/user")
-async def post_user(user: User):
-    return {"request body": user}
+@app.post("/user", status_code=HTTP_201_CREATED)
+async def post_user(user: User, x_custom: str = Header(...)):
+    return {"request body": user, "request custom header": x_custom}
 
 
 @app.get("/user")
@@ -16,12 +17,24 @@ async def get_user_validation(password: str):
     return {"query parameter": password}
 
 
-@app.get("/book/{isbn}")
+@app.get("/book/{isbn}", response_model=Book, response_model_exclude=["author"]) #or use response_model_include=["",""]
 async def get_book_with_isbn(isbn: str):
-    return {"query changeable parameter": isbn}
+    author_dict = {
+        "name": "Canberk",
+        "book": ["book1", "book2"]
+    }
+    author1 = Author(**author_dict)
+    book_dict = {
+        "isbn":"1234",
+        "name":"book1",
+        "year":"2019",
+        "author": author1
+    }
+    book1 = Book(**book_dict)
+    return book1
 
 
-@app.get("/author/{id}/book")
+@app.get("/author/{idk}/book")
 async def get_authors_books(
     idk: int, category: str, order: str = "asc",
 ):
